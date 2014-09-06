1e518cd2d5a9a710cdb9f4e1bd19b04b9b93cf0b
Lowe Thiderman <lowe.thiderman@gmail.com>
2014-09-06 16:53:57 +0200

    Refactor parameter handling

--- http_remote.py
+++ http_remote.py
@@ -7,7 +7,6 @@ import json
 
 # Default port that Spotify Web Helper binds to.
 PORT = 4371
-ORIGIN_HEADER = {'Origin': 'https://open.spotify.com'}
 
 
 # I had some troubles with the version of Spotify's SSL cert and Python 2.7 on
@@ -20,6 +19,9 @@ orig_wrap_socket, ssl.wrap_socket = ssl.wrap_socket, new_wrap_socket
 
 
 class SpotifyCLI(object):
+    oauth_token = None
+    csrf_token = None
+
     def setup(self):
         self.domain = '{0}.spotilocal.com'.format(
             ''.join(random.choice(string.ascii_lowercase) for x in range(10))
@@ -32,65 +34,45 @@ class SpotifyCLI(object):
         return self.get_json('http://open.spotify.com/token')['t']
 
     def get_csrf_token(self):
-        # Requires Origin header to be set to generate the CSRF token.
-        ret = self.get_json('/simplecsrf/token.json', headers=ORIGIN_HEADER)
+        ret = self.get_json('/simplecsrf/token.json')
         return ret['token']
 
     def get_json(self, url, params={}, headers={}):
         if url.startswith('/'):
             url = "https://%s:%d%s" % (self.domain, PORT, url)
 
-        if params:
-            url += "?" + urllib.urlencode(params)
+        # Always add the default parameters and headers
+        params.update({
+            'oauth': self.oauth_token,
+            'csrf': self.csrf_token,
+        })
+        headers.update({
+            'Origin': 'https://open.spotify.com'
+        })
+
+        url += "?" + urllib.urlencode(params)
 
         request = urllib2.Request(url, headers=headers)
         return json.loads(urllib2.urlopen(request).read())
 
     def get_version(self):
-        return self.get_json(
-            '/service/version.json',
-            params={'service': 'remote'},
-            headers=ORIGIN_HEADER
-        )
+        return self.get_json('/service/version.json', {'service': 'remote'})
 
     def get_status(self):
-        params = {
-            'oauth': self.oauth_token,
-            'csrf': self.csrf_token,
-        }
-        return self.get_json(
-            '/remote/status.json',
-            params=params,
-            headers=ORIGIN_HEADER
-        )
+        return self.get_json('/remote/status.json')
 
     def pause(self, pause=True):
-        params = {
-            'oauth': self.oauth_token,
-            'csrf': self.csrf_token,
-            'pause': 'true' if pause else 'false'
-        }
-        self.get_json(
-            '/remote/pause.json',
-            params=params,
-            headers=ORIGIN_HEADER
-        )
+        pause = 'true' if pause else 'false'
+        self.get_json('/remote/pause.json', {'pause': pause})
 
     def unpause(self):
         self.pause(pause=False)
 
     def play(self, spotify_uri):
-        params = {
-            'oauth': self.oauth_token,
-            'csrf': self.csrf_token,
+        self.get_json('/remote/play.json', {
             'uri': spotify_uri,
-            'context': spotify_uri,
-        }
-        self.get_json(
-            '/remote/play.json',
-            params=params,
-            headers=ORIGIN_HEADER
-        )
+            'context': spotify_uri
+        })
 
 if __name__ == '__main__':
     spotify = SpotifyCLI()
